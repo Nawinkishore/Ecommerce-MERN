cb75cb8d96ead6e139213248b8240218a2f2ec43
Nawinkishore <nawinkishore2004@gmail.com>
2025-01-13 02:51:04 +0530

    Feat : Loader Updated

--- Frontend/src/components/layout/Loader.jsx
+++ Frontend/src/components/layout/Loader.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 const Loader = () => {
     return (
         <div>
-            <span className="loading loading-ring loading-xs"></span>
-            <span className="loading loading-ring loading-sm"></span>
-            <span className="loading loading-ring loading-md"></span>
             <span className="loading loading-ring loading-lg"></span>
         </div>
     );
